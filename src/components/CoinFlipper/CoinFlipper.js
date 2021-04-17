b2c214ba12cf768a102342c93e61ae251456cff2
Ozan Uludağ <ozann6446@gmail.com>
2021-04-17 22:37:49 +0300

    first commit

--- src/components/CoinFlipper/CoinFlipper.js
+++ src/components/CoinFlipper/CoinFlipper.js
@@ -8,29 +8,44 @@ class CoinFlipper extends Component {
     // State üzerinde paranın başlangıçtaki durumunu veriyoruz, başlangıçta "tura" olsun.
     // Daha sonra şu anda paranın dönüp dönmeme durumunu da veriyoruz, başlangıçta para atılmamış olduğundan "false" olarak verdik.
     this.state = {
-      side: "tura",
+      yazi: 0,
+      tura: 0,
       flipping: false,
+      counter: 0,
+      currentNumber: 0
     };
   }
   handleClick = () => {
     // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "flipping" durumunu "true" yapıyoruz.
     this.setState({ flipping: true });
     // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "flipping" durmunu tekrar "false" yapıyoruz.
-    setTimeout(() => this.setState({ flipping: false }), 1000);
+
+    setTimeout(() => {
+      let number = Math.random();
+      //console.log(number);
+      number > 0.5
+        ? this.setState({ tura: this.state.tura + 1 })
+        : this.setState({ yazi: this.state.yazi + 1 });
+      this.setState({ currentNumber: number });
+      this.setState({ counter: this.state.counter + 1 });
+      this.setState({ flipping: false })
+    }
+    , 1000);
+
   };
 
   render() {
     return (
       <div className="CoinFlipper">
         <h1>Yazı mı Tura mı?</h1>
-        <Coin side={this.state.side} flipping={this.state.flipping} />
+        <Coin side={this.state.currentNumber < 0.5 ? "yazi" : null} flipping={this.state.flipping} />
         <button onClick={this.handleClick}>At!</button>
         <p>
           Toplam
-          <strong> 5 </strong>
+          <strong> {this.state.counter} </strong>
           atıştan
-          <strong> 3 </strong>ü tura
-          <strong> 2 </strong>
+          <strong> {this.state.tura} </strong>ü tura
+          <strong> {this.state.yazi} </strong>
           si yazı geldi.
         </p>
       </div>
